6a5d22536927b3f22d679b1c3639b3d9627c3e7a
Alexandre Souza <alexandre_@fastmail.com>
2015-05-12 21:43:51 -0300

    experimentando loops infinitos

---
+++ experimentos/infinito.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	for {
+		fmt.Println("Loop infinito omitindo tudo!")
+	}
+}
+
+func main() {
+	for true {
+		fmt.Println("Loop infinito parecendo while")
+	}
+}
